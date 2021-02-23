1dc37d932e123c9412fb01677dea67c78ff733a4
NAGA RAGHAVENDRA <76747621+Naga0529@users.noreply.github.com>
2021-02-23 16:44:59 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ABC'
-copyright = '2021, Harideep'
-author = 'Harideep'
+project = 'TechDocs'
+copyright = '2021, Raghav'
+author = 'Raghav'
 
 # The full version, including alpha/beta/rc tags
 release = '2021'
